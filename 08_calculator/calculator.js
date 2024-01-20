b846d9a66cff8089369e9d8a6b77a2c656192df3
Tyler Zanon <tdzanon@gmail.com>
2024-01-19 21:31:36 -0500

    done exercise 8

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,32 @@
-const add = function() {
-	
+const add = function(a, b) {
+	return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	return a - b;
 };
 
-const sum = function() {
-	
+const sum = function(arr) {
+	return arr.reduce((sum, current) => sum + current, 0);
 };
 
-const multiply = function() {
-
+const multiply = function(arr) {
+  return arr.reduce((product, current) => product * current, 1);
 };
 
-const power = function() {
-	
+const power = function(a, b) {
+	return a ** b;
 };
 
-const factorial = function() {
-	
+const factorial = function(n) {
+  if (n <= 1) return 1;
+
+	let f = 1;
+  for (let i = 2; i <= n; i++) {
+    f *= i;
+  }
+  
+  return f;
 };
 
 // Do not edit below this line
